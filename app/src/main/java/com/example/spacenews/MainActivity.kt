e0c5625fe6d780a6a936515d3c784af00cef84a2
kanake10 <10kanake@gmail.com>
2023-02-28 13:59:50 +0300

    launch activity

--- app/src/main/java/com/example/spacenews/MainActivity.kt
+++ app/src/main/java/com/example/spacenews/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.spacenews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.navigation.compose.NavHost
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.spacenews.ui.theme.Screen
+import com.example.spacenews.ui.theme.SpaceNewsScreen
 import com.example.spacenews.ui.theme.SpaceNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,29 +20,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             SpaceNewsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("kanake")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.ArticleScreen.route
+                    ) {
+                        composable(
+                            route = Screen.ArticleScreen.route
+                        ) {
+                            SpaceNewsScreen(navController)
+                        }
+//                        composable(
+//                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+//                        ) {
+//                            CoinDetailScreen()
+//                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpaceNewsTheme {
-        Greeting("Android")
     }
 }
