af159ae5146d82276cca098ebeee757c36220309
kanake10 <10kanake@gmail.com>
2023-03-02 16:58:58 +0300

    log okhttp data correctly from api calls

--- app/src/main/java/com/example/spacenews/di/AppModule.kt
+++ app/src/main/java/com/example/spacenews/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.example.spacenews.di
 
+import com.example.spacenews.core.Constants
 import com.example.spacenews.core.Constants.BASE_URL
 import com.example.spacenews.data.SpaceNewsApi
 import com.example.spacenews.data.SpaceNewsRepoImpl
@@ -13,35 +14,44 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
+    @Singleton
     @Provides
-    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     }
 
     @Provides
-    fun providesOkhttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .callTimeout(60, TimeUnit.SECONDS)
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+
         return okHttpClient.build()
     }
 
     @Provides
-    fun provideApi(): SpaceNewsApi {
+    @Singleton
+    fun provideApi(okHttpClient: OkHttpClient): SpaceNewsApi {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(SpaceNewsApi::class.java)
     }
+
+
+    @Singleton
     @Provides
     fun provideRepository(spaceNewsApi: SpaceNewsApi): SpaceNewsRepo {
         return SpaceNewsRepoImpl(spaceNewsApi)
