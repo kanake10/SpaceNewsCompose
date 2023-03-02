1bccbe6dddb1ba9bb97a6d5d2894e7bb8c1cd7e7
kanake10 <10kanake@gmail.com>
2023-03-02 17:18:16 +0300

    use one slash

--- app/src/main/java/com/example/spacenews/core/Constants.kt
+++ app/src/main/java/com/example/spacenews/core/Constants.kt
@@ -3,7 +3,7 @@ package com.example.spacenews.core
 object Constants {
     const val BASE_URL ="https://api.spaceflightnewsapi.net/v3/"
     const val ARTICLES_ENDPOINT ="articles"
-    const val ARTICLES_DETAILS_ENDPOINT ="/articles/{id}"
+    const val ARTICLES_DETAILS_ENDPOINT ="articles/{id}"
     const val PARAM_ID = "id"
 }
 
